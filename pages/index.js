47e130bc280eabe6b09997e96c001c38e54164a2
Mike Nguyen <nguyenduchung0609@gmail.com>
2020-07-07 07:02:08 +1200

    Get Products from MongoDB and display in Card

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
 
 function Home({ products }) {
-  console.log(products);
-
-  // React.useEffect(() => {
-  //   getProducts()
-  // }, [])
-  return <>home</>
+  
+  return <ProductList products={products} />
 }
-
-// async function getProducts() {
-//   const url = 'http://localhost:3000/api/products';
-//   const response = await axios.get(url);
-//   console.log(response.data);  
-// }
 
 Home.getInitialProps = async () => {
   // fetch data on the server
