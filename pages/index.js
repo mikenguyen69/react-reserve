cb22dd8089281575183d06b11989ee33e52d50c4
Mike Nguyen <nguyenduchung0609@gmail.com>
2020-07-07 05:35:56 +1200

    Fetching data from API (mock) on server vs. client!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-function Home() {
-  return <>home</>;
+import React from 'react';
+import axios from 'axios';
+
+function Home({ products }) {
+  console.log(products);
+
+  // React.useEffect(() => {
+  //   getProducts()
+  // }, [])
+  return <>home</>
+}
+
+// async function getProducts() {
+//   const url = 'http://localhost:3000/api/products';
+//   const response = await axios.get(url);
+//   console.log(response.data);  
+// }
+
+Home.getInitialProps = async () => {
+  // fetch data on the server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+
+  // return response data as an object
+  return { products: response.data };
+  // note: these object will be merge with existing props
 }
 
 export default Home;
